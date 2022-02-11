9feca30fd6f6acc7849927d963ce8c87439a9d3f
ちくしの <vipqiv00+chixino@gmail.com>
2022-02-11 17:34:41 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    let value = "world";
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
+
+<h1>Hello {value} !</h1>
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: process.env.NODE_ENV || "development",
+    module: {
+        rules: [
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: "svelte-loader",
+                    options: {
+                        // tureにすることで、
+                        // svelteファイルの<style>タグをのcssを
+                        // 「test: /\.css$/」のフローでコンパイルできる
+                        emitCss: true,
+                    }
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                ],
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".mjs", ".js", ".svelte"],
+    },
+    plugins: [
+        // 出力する際のファイル名を指定する
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")
+        },
+        open: true
+    }
+};
