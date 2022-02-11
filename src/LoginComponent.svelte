9094546a2c251c5752e7506d97ab86c25cdd5b0e
ちくしの <vipqiv00+chixino@gmail.com>
2022-02-12 00:39:51 +0900

    FIX: change to redirect

--- src/LoginComponent.svelte
+++ src/LoginComponent.svelte
@@ -1,8 +1,4 @@
 <script>
-    import {get} from "./utils";
-    const loginUrl = get("/login");
 </script>
 
-{#await loginUrl then url}
-    <a href={url.body}>ログイン</a>
-{/await}
+<a href="https://6wx2hlt0bd.execute-api.ap-northeast-1.amazonaws.com/dev/login">ログイン</a>
