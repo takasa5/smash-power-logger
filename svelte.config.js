102e2eacbe701631a643eaa593dbaf7af8624046
ちくしの <vipqiv00+chixino@gmail.com>
2022-02-12 11:28:50 +0900

    FIX: static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		})
 	}
 };
-
-export default config;
