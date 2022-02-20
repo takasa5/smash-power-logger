25bbe050c94f4cee19ec7057f5e2c9fcdbcee75d
takasa5 <vipqiv00@gmail.com>
2022-02-20 16:09:52 +0900

    ADD: ユーザー作成

--- src/routes/callback.js
+++ src/routes/callback.js
@@ -46,17 +46,64 @@ export async function get({ url, locals }) {
         }
     }
 
-    // アクセストークンをCookieに保存する
+    const { data: userObj } = await userClient.v2.me({
+        "user.fields": 'profile_image_url'
+    });
+    // 必要であればユーザーを新規作成する
+    const splId = createUserIfNotExist(userObj);
+    userObj.splId = splId;
+    // アクセストークンとユーザ情報をCookieに保存する
     // TODO: 暗号化
-    const userObj = await userClient.v2.me();
     locals.user = {
         token: accessToken,
         info: userObj
     };
+
     return {
         status: 302,
         headers: {
             location: "/"
         }
+    }
+}
+
+async function createUserIfNotExist(userObj) {
+    try {
+        const { Items: result } = await ddb.query({
+            TableName: "SmashPowerLoggerUser",
+            IndexName: "twitter_id-index",
+            KeyConditionExpression: "twitter_id = :twitterId",
+            ExpressionAttributeValues: {
+                ":twitterId": userObj.id
+            }
+        }).promise();
+        if (result.length === 0) {
+            // 新規登録ユーザー
+            const { Attributes: seq } = await ddb.update({
+                TableName: "Sequences",
+                Key: {
+                    table_name: "SmashPowerLoggerUser"
+                },
+                UpdateExpression: "set current_number = current_number + :value",
+                ExpressionAttributeValues: {
+                    ":value": 1
+                },
+                ReturnValues: "UPDATED_NEW"
+            }).promise();
+            await ddb.put({
+                TableName: "SmashPowerLoggerUser",
+                Item: {
+                    id: seq.current_number,
+                    twitter_id: userObj.id,
+                    twitter_username: userObj.username,
+                    twitter_name: userObj.name,
+                    twitter_image: userObj.profile_image_url
+                }
+            }).promise();
+            return seq.current_number;
+        }
+        return result[0].id;
+    } catch (err) {
+        console.log(err);
     }
 }
