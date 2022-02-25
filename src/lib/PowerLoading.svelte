f0ef5059d74afa29f3bdd84d3fb7a54ec04cd8e7
takasa5 <vipqiv00@gmail.com>
2022-02-25 14:48:30 +0900

    UPDATE: モーダル取得処理周りUI整備

--- src/lib/PowerLoading.svelte
+++ src/lib/PowerLoading.svelte
@@ -1,20 +1,24 @@
 <script>
+    import moment from "moment";
     import { getContext } from "svelte";
     const { close } = getContext("simple-modal");
     export let splId;
+    let powers = [];
 
     async function getPower() {
         const response = await fetch(`/users/${splId}/powers`, {
             method: "GET"
         });
         const res = await response.json();
-        console.log(res);
         // TODO: 取得時に期限付きでどっかのストレージに入れられたらよさそう
+        powers = res;
         return res;
     }
 
     async function getPowerDummy() {
-        return [
+        const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+        await sleep(3000);
+        powers = [
             {
                 id: '3_1495754719251238914',
                 power: '11138316',
@@ -26,16 +30,32 @@
                 power: '11140404',
                 fighter: 'キャフテン・ファルコン',
                 time: '2022-02-23T13:38:09.000Z'
+            },
+            {
+                id: '3_1495754719251238944',
+                power: '11145098',
+                fighter: 'キャフテン・ファルコン',
+                time: '2022-02-28T13:38:09.000Z'
             }
-        ];
+        ]
+        return powers;
+    }
+
+    function deleteData(mediaId) {
+        powers = powers.filter(e => e.id != mediaId);
     }
 </script>
 
 {#await getPowerDummy()}
-    <p>Loading...</p>
+    <div class="Box">
+        <div class="blankslate">
+            <h3 class="blankslate-heading">ツイートから戦闘力を読み込んでいます<span class="AnimatedEllipsis"></span></h3>
+            <p>これには数秒かかることがあります</p>
+        </div>
+    </div>
 {:then datas}
     <div class="Box">
-    {#each datas as data}
+    {#each powers as data (data.id)}
         <div class="Box-row d-flex flex-items-center">
             <div class="flex-auto d-flex flex-column flex-md-row">
                 <div class="col-2 d-flex flex-justify-center flex-items-center">
@@ -44,11 +64,16 @@
                 <div>
                     <div class="h2">{data.power}</div>
                     <div class="text-small color-fg-subtle">
-                        {data.time}
+                        {moment(data.time, "YYYY-MM-DDTHH:mm:ssZ").format("YYYY/MM/DD HH:mm:ss")}
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn">削除</button>
+            <button type="button" class="btn" on:click={deleteData(data.id)}>記録しない</button>
+        </div>
+    {:else}
+        <div class="blankslate">
+            <h3 class="blankslate-heading">記録する戦闘力が見つかりません</h3>
+            <p>ヘルプページを参照してください</p>
         </div>
     {/each}
     </div>
@@ -56,7 +81,7 @@
         <button class="btn btn-secondary mr-2" on:click={close}>
             キャンセル
         </button>
-        <button class="btn btn-primary">
+        <button class="btn btn-primary" aria-disabled={powers.length == 0}>
             記録する
         </button>
     </div>
